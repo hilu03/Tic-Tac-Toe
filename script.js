1476ee50b014c07cc963202cd0f23d1a8ecab27f
Luong Tran <tranthihienluong2003@gmail.com>
2024-08-21 23:12:01 +0700

    Play with UI

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function Gameboard() {
   const row = 3;
   const column = 3;
   const board = [];
+  let moveCount = 0;
 
   for (let i=0; i<row; i++) {
     board[i] = [];
@@ -16,18 +17,21 @@ function Gameboard() {
 
   const getCell = (rowIndex, columnIndex) => board[rowIndex][columnIndex];
 
+  const getMoveCount = () => moveCount;
+
   const drawMark = (rowIndex, columnIndex, mark) => {
     if (checkAvailableCell(rowIndex, columnIndex)) {
       board[rowIndex][columnIndex] = mark;
+      moveCount++;
     }
   }
 
-  return { getBoard, drawMark, getCell };
+  return { getBoard, drawMark, getCell, getMoveCount };
 }
 
 function GameController(player1 = "X", player2 = "O") {
   const board = Gameboard();
-  let moveCount = 0;
+  let result = -1;
 
   const players = [
     {
@@ -49,7 +53,6 @@ function GameController(player1 = "X", player2 = "O") {
   const getActivePlayer = () => activePlayer;
 
   const printNewRound = () => {
-    moveCount++;
     console.log(board.getBoard());
     console.log(`${activePlayer.name}'s turn.`);
   }
@@ -61,11 +64,13 @@ function GameController(player1 = "X", player2 = "O") {
     if (checkWinner(rowIndex, columnIndex, activePlayer.mark)) {
       console.log(board.getBoard());
       console.log(`${activePlayer.name} wins!`);
+      result = 1;
       return;
     }
-    else if (moveCount === 9) {
+    else if (board.getMoveCount() === 9) {
       console.log(board.getBoard());
       console.log("It's a tie!");
+      result = 0;
       return;
     }
     switchPlayerTurn();
@@ -106,13 +111,13 @@ function GameController(player1 = "X", player2 = "O") {
       }
     }
     
-    if (rowIndex + columnIndex === 4) {
+    if (rowIndex + columnIndex === 2) {
       //anti-diag
       for (let i = 0; i < 4; i++) {
         if (i === 3) {
           return true;
         }
-        if (board.getCell(i, 4 - i) !== mark) {
+        if (board.getCell(i, 2 - i) !== mark) {
           break;
         }
       }
@@ -120,18 +125,57 @@ function GameController(player1 = "X", player2 = "O") {
     return false;
   };
 
-  return { playRound };
+  const getResult = () => result;
+
+  return { playRound, getActivePlayer, getResult };
 }
 
 function DisplayController() {
+  const game = GameController();
   const renderGameboard = () => {
     const board = document.querySelector(".board");
     let html = "";
-    for (let i = 0; i < 9; i++) {
-      html += "<div class='cell'></div>";
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        html += `<div class='cell' data-row='${i}' data-column='${j}'></div>`;
+      }
     }
     board.innerHTML = html;
+    displayPlayerTurn();
+
+    const gridCells = document.querySelectorAll(".cell");
+    gridCells.forEach(cell => {
+      cell.addEventListener("click", () => {
+        if (cell.textContent.length === 0 && game.getResult() === -1) {
+          cell.textContent = game.getActivePlayer().mark;
+          cell.classList.add(game.getActivePlayer().mark);
+          game.playRound(Number(cell.dataset.row), Number(cell.dataset.column));
+          if (game.getResult() !== -1) {
+            displayResult();
+          }
+          else {
+            displayPlayerTurn(); 
+          } 
+        }
+      });
+    });  
   };
+
+  const displayPlayerTurn = () => {
+    const turn = document.querySelector(".turn");
+    turn.textContent = `${game.getActivePlayer().name}'s turn.`;
+  }
+
+  const displayResult = () => {
+    const result = document.querySelector(".result");
+    if (game.getResult() === 0) {
+      result.textContent = "It'a tie!";
+    }
+    else {
+      result.textContent = `We got the winner. ${game.getActivePlayer().name} wins!`;
+    }
+  };
+
 
   return { renderGameboard };
 }
